dc2e438441bdab1d7aa90acbdfb843669686386c
Kenan <kenan.binay@hotmail.com>
2022-11-18 23:55:11 +0300

    new assests & missile controller script & joystick

--- Assets/scripts/CamController.cs
+++ Assets/scripts/CamController.cs
@@ -9,7 +9,7 @@ public class CamController : MonoBehaviour
 
     public float smoothSpeed;
 
-    bool startDelayed;
+    public static bool startDelayed;
     void Start()
     {     
         startDelayed = false;
@@ -21,11 +21,8 @@ public class CamController : MonoBehaviour
     {
         if (startDelayed)
         {
-            Vector3 desiredPosition = new Vector3(targetMissile.transform.position.x, targetMissile.transform.position.y, targetMissile.transform.position.z);
-            Vector3 SmoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-
-            transform.position = new Vector3(targetMissile.transform.position.x, targetMissile.transform.position.y, targetMissile.transform.position.z);
-            transform.Rotate(new Vector3(targetMissile.transform.rotation.x, targetMissile.transform.rotation.y, targetMissile.transform.rotation.z));
+      //      transform.position = new Vector3(targetMissile.transform.position.x, targetMissile.transform.position.y, targetMissile.transform.position.z);
+       //     transform.Rotate(new Vector3(targetMissile.transform.rotation.x, targetMissile.transform.rotation.y, targetMissile.transform.rotation.z));
         }
     }
 
--- Assets/scripts/missileController.cs
+++ Assets/scripts/missileController.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class missileController : MonoBehaviour
 {
+    public static Vector2 handleInput;
+
+    public float flySpeed, yawAmount;
+    float yaw, pitch;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+        if (CamController.startDelayed)
+        {
+            //move forward
+            transform.position += transform.forward * flySpeed * Time.deltaTime;
+
+            //input
+            yaw += handleInput.x * yawAmount * Time.deltaTime;
+            pitch += handleInput.y * yawAmount * Time.deltaTime;
+
+            //apply rotation
+            transform.localRotation = Quaternion.Euler(Vector3.up * yaw + Vector3.right * pitch);
+        }
     }
 }
