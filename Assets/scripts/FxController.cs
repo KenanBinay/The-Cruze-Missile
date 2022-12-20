af4927da97b94241d4413ced8d2b5e07f422cca2
Kenan <kenan.binay@hotmail.com>
2022-12-21 01:24:21 +0300

    recorder asset added & fxExplosion script bug fix

--- Assets/scripts/FxController.cs
+++ Assets/scripts/FxController.cs
@@ -10,38 +10,32 @@ public class FxController : MonoBehaviour
 
     void Start()
     {
-        
+        fxExplode = false;
     }
 
     private void Update()
     {
-        
-    }
+        if (missileController.crashed || missileController.targetHit && !fxExplode)
+        {
+            int[] Fx_targetHit = { 0, 1, 2, 3, 4 };
+            int selectedTargetHit_Fx = Fx_targetHit[Random.Range(0, Fx_targetHit.Length)];
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        int[] Fx_targetHit = { 0, 1, 2, 3, 4 };
-        int selectedTargetHit_Fx = Fx_targetHit[Random.Range(0, Fx_targetHit.Length)];
+            int[] Fx_crash = { 1, 2 };
+            int selectedCrash_Fx = Fx_crash[Random.Range(0, Fx_crash.Length)];
 
-        int[] Fx_crash = { 1, 2 };
-        int selectedCrash_Fx = Fx_crash[Random.Range(0, Fx_crash.Length)];
+            if (missileController.crashed && !fxExplode)
+            {
+                explosionParticle_crash[selectedCrash_Fx].SetActive(true);
+                explosionParticle_crash[0].SetActive(true);
 
-        if (collision.gameObject.CompareTag("crashColl"))
-        {
-            explosionParticle_crash[selectedCrash_Fx].SetActive(true);
-            explosionParticle_crash[0].SetActive(true);
-        }
-        if (collision.gameObject.CompareTag("target"))
-        {
-            explosionParticle_hit[selectedTargetHit_Fx].SetActive(true);
-        }
-        if (collision.gameObject.CompareTag("vehicleTarget"))
-        {
-            explosionParticle_hit[selectedTargetHit_Fx].SetActive(true);
-        }
-        if (collision.gameObject.CompareTag("airTarget"))
-        {
-            explosionParticle_hit[selectedTargetHit_Fx].SetActive(true);
+                fxExplode = true;
+            }
+            if (missileController.targetHit && !fxExplode)
+            {
+                explosionParticle_hit[selectedTargetHit_Fx].SetActive(true);
+
+                fxExplode = true;
+            }
         }
     }
 }
