feee4ebfcab886597ca685135ec6ff417469c5e6
Kenan <kenanbinay226@gmail.com>
2023-02-13 00:07:35 +0300

    draw line script drawing bug fix

--- Assets/_scripts/drawLine.cs
+++ Assets/_scripts/drawLine.cs
@@ -10,8 +10,7 @@ public class drawLine : MonoBehaviour
     public Transform missile;
     public Transform target;
 
-    public float lineDrawSpeed = 6f;
-    public float renderMeterLimit;
+    public float lineDrawSpeed = 6f, renderMeterLimit, targetDistance;
 
     void Start()
     {
@@ -27,13 +26,18 @@ public class drawLine : MonoBehaviour
             if (targetController.target_type == 2) { target = propAircraftController.aircraft.GetComponent<Transform>(); }
         }
 
-        if (target != null && gameController.startDelay)
+        if (gameController.startDelay)
         {
-            if (!missileController.crashed && !missileController.targetHit && MissionWaypoint.targetMeter < renderMeterLimit)
+            if (!missileController.crashed && !missileController.targetHit)
             {
-                drawingGuideLine();
+                targetDistance = Vector3.Distance(missile.position, target.position);
+                if (targetDistance < renderMeterLimit)
+                {
+                    Debug.Log("guide lineRender");
+                    drawingGuideLine();
+                }
             }
-            if (missileController.crashed || missileController.targetHit || MissionWaypoint.targetMeter > renderMeterLimit)
+            if (missileController.crashed || missileController.targetHit || targetDistance > renderMeterLimit)
             {
                 Vector3 lineZero = lineRenderer.GetPosition(0);
                 if (lineZero != new Vector3(0, 0, 0))
@@ -50,16 +54,14 @@ public class drawLine : MonoBehaviour
 
         dist = Vector3.Distance(missile.position, target.position);
         float x = Mathf.Lerp(0, dist, counter);
-
+    
         Vector3 pointA = missile.position;
         Vector3 pointB = target.position;
 
         Vector3 pointALongLine = x * Vector3.Normalize(pointB - pointA) + pointA;
 
         lineRenderer.SetPosition(0, pointA);
-        lineRenderer.SetPosition(1, pointALongLine);
-
-     //   Debug.Log("target guide line render");
+        lineRenderer.SetPosition(1, pointB);
     }
 
     void endGuideLine()
