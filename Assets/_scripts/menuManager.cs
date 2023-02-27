f9777da2052b6016c974447090ca937f0b69320a
Kenan <kenanbinay226@gmail.com>
2023-02-27 20:19:15 +0300

    menuManager missile selection indicators & change button load MissileSelectionMenu

--- Assets/_scripts/menuManager.cs
+++ Assets/_scripts/menuManager.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 public class menuManager : MonoBehaviour
 {
     public int sceneId;
-    bool waitForReload;
+    int selectedMissileNumb;
+    bool waitForReload, missileMenuOpened;
 
     public Animator bottomStart;
+    public GameObject startMenu_canvas, missileMenu_canvas, missileSelectionBoxes
+        , bar_missileMenu, barOpened_missileMenu;
 
     void Start()
     {
@@ -17,7 +20,10 @@ public class menuManager : MonoBehaviour
 
     void Update()
     {
-
+        if (missileMenuOpened)
+        {
+            
+        }
     }
 
     public void loadMission()
@@ -27,6 +33,63 @@ public class menuManager : MonoBehaviour
             bottomStart.SetTrigger("startClick");
             AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
             if (!operation.isDone) { waitForReload = true; }            
+        }
+    }
+
+    public void loadMissileChangeMenu()
+    {
+        missileMenuOpened = true;
+        startMenu_canvas.SetActive(false);
+        missileMenu_canvas.SetActive(true);
+        bar_missileMenu.SetActive(true);
+        barOpened_missileMenu.SetActive(false);
+
+        for (int a = 0; a < 6; a++)
+        {
+            GameObject missileSelected = missileSelectionBoxes.transform.Find("missileBox_" + a)
+                .gameObject;
+            GameObject selectIndicator = missileSelected.transform.GetChild(1).gameObject;
+                selectIndicator.SetActive(false);
+        }
+    }
+
+    public void selectMissile(int missileNumb)
+    {
+        for (int a = 0; a < 6; a++)
+        {
+            GameObject missileSelected = missileSelectionBoxes.transform.Find("missileBox_" + a)
+                .gameObject;
+            GameObject selectIndicator = missileSelected.transform.GetChild(1).gameObject;
+
+            if (a == missileNumb)
+                selectIndicator.SetActive(true);
+
+            if (a != missileNumb && selectIndicator.activeSelf)
+                selectIndicator.SetActive(false);
+        }
+
+        selectedMissileNumb = missileNumb;
+        Debug.Log("missile selected " + missileNumb);
+
+        StartCoroutine(onMissileSelected());
+    }
+
+    IEnumerator onMissileSelected()
+    {
+        if (selectedMissileNumb == 0)
+        {
+            missileMenuOpened = false;
+
+            yield return new WaitForSeconds(0.3f);
+
+            startMenu_canvas.SetActive(true);
+            missileMenu_canvas.SetActive(false);
+        }
+
+        if (selectedMissileNumb != 0)
+        {
+            bar_missileMenu.SetActive(false);
+            barOpened_missileMenu.SetActive(true);
         }
     }
 }
