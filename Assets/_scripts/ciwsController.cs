948c0f7d4cca9a6f096465fe6cb06f79f0213724
Kenan <kenanbinay226@gmail.com>
2023-03-05 21:30:18 +0300

    quality settings cahnge & postProccesing change

--- Assets/_scripts/ciwsController.cs
+++ Assets/_scripts/ciwsController.cs
@@ -17,48 +17,51 @@ public class ciwsController : MonoBehaviour
         roundEffect.SetActive(false);
 
         DOTween.SetTweensCapacity(3200, 60);
-      
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("missileM") && ciwsSpawner.ciwsSpawned) { targetDetected = true; Debug.Log("lockedOn"); }      
+        if (other.gameObject.CompareTag("missileM") && ciwsSpawner.ciwsSpawned)
+            targetDetected = true; Debug.Log("lockedOn");
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("missileM") && !missileController.crashed && !missileController.targetHit && ciwsSpawner.ciwsSpawned)
+        if (other.gameObject.CompareTag("missileM") && !missileController.crashed
+            && !missileController.targetHit && ciwsSpawner.ciwsSpawned)
         {
             roundEffect.SetActive(true);
 
-            if (gunM != null) gunM.transform.DOLookAt(new Vector3(0, missile.transform.position.y, 0), 3);
-            if (gunUp != null) gunUp.transform.DOLookAt(missile.transform.position, 3);
+            gunM.transform.DOLookAt(new Vector3(0, missile.transform.position.y, 0), 3);
+            gunUp.transform.DOLookAt(missile.transform.position, 3);
         }
-        if (missileController.crashed || missileController.targetHit)
+        if (other.gameObject.CompareTag("missileM")
+            && missileController.crashed || missileController.targetHit)
         {
-            returnStatic();
+            StartCoroutine(returnStatic());
         }
     }
 
     public void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("missileM")) 
+        if (other.gameObject.CompareTag("missileM"))
         {
             Debug.Log("lockedOff");
             targetDetected = false;
-            returnStatic();
+
+            StartCoroutine(returnStatic());
         }
     }
 
-    void returnStatic()
+    IEnumerator returnStatic()
     {
-        if (missileController.crashed || missileController.targetHit || !targetDetected)
-        {
-            gunM.transform.DORotate(new Vector3(0, 0, 0), 0.5f);
-            gunUp.transform.DORotate(new Vector3(0, 0, 0), 0.5f);
+        gunM.transform.DORotate(new Vector3(0, 0, 0), 0.5f);
+        gunUp.transform.DORotate(new Vector3(0, 0, 0), 0.5f);
 
-            roundEffect.SetActive(false);
-            targetDetected = false;
-        }
+        roundEffect.SetActive(false);
+
+        yield return new WaitForSeconds(1);
+
+        DOTween.Complete(gunUp, gunM);
     }
 }
