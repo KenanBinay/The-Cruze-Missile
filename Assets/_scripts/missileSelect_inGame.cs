db6bcc728d1148409654410eadcd396c7a3431ab
Kenan <kenanbinay226@gmail.com>
2023-04-10 17:52:25 +0300

    removeAds icon, pos change & missileSelect_inGame script selected missile now instantiated from prefabs

--- Assets/_scripts/missileSelect_inGame.cs
+++ Assets/_scripts/missileSelect_inGame.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class missileSelect_inGame : MonoBehaviour
 {
     GameObject missile;
+    [SerializeField] GameObject spawnedMissile;
+    [SerializeField] GameObject[] missilePrefabs;
+    [SerializeField] GameObject[] premium_missilePrefabs;
+
     void Start()
     {
         if (missile != null) { missile.SetActive(false); }
 
         int lastSelected = PlayerPrefs.GetInt("lastSelectedMissile");
 
-        missile = gameObject.transform.GetChild(lastSelected).gameObject;
+        //     missile = gameObject.transform.GetChild(lastSelected).gameObject;
+        int layerNumb = gameObject.layer;
+        string nameLayer = LayerMask.LayerToName(layerNumb);
 
-        missile.SetActive(true);
+        GameObject spawnedMissile = Instantiate(missilePrefabs[lastSelected], transform);
+
+        spawnedMissile.layer = LayerMask.NameToLayer(nameLayer);
+        Debug.Log(spawnedMissile.layer);
+
+        foreach (Transform child in spawnedMissile.transform)
+        {
+            child.gameObject.layer = LayerMask.NameToLayer(nameLayer); ;        
+        }
+        //     missile.SetActive(true);
 
         Debug.Log("selected missile : " + lastSelected);
     }
